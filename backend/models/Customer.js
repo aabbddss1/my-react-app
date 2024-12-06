80dde543ebc5e3fe549d13ba0b189b4eb018779e
Samet <74731602+aabbddss1@users.noreply.github.com>
2024-12-06 15:42:07 +0100

    Update Customer.js

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const customerSchema = new mongoose.Schema({
+const CustomerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
   address: { type: String, required: true },
-});
+  city: { type: String, required: true },
+  postalCode: { type: String, required: true },
+}, { timestamps: true });
 
-module.exports = mongoose.model('Customer', customerSchema);
+module.exports = mongoose.model('Customer', CustomerSchema);
